192d7f1147c4f7c5ffe395b26a6d7d6a0794ff97
WangZhisong <2659460681@qq.com>
2022-09-28 16:44:16 +0800

    优化代码

--- handlers/hello/HelloHandler.go
+++ handlers/hello/HelloHandler.go
@@ -8,3 +8,39 @@ func Hello(context *gin.Context) {
 		"msg":  "hello,world",
 	})
 }
+
+func QuickSort(arr []int) []int {
+	if len(arr) <= 1 {
+		return arr
+	}
+	first := arr[0]
+	low := make([]int, 0, 0)
+	high := make([]int, 0, 0)
+	mid := make([]int, 0, 0)
+	mid = append(mid, first)
+	for i := 0; i < len(arr); i++ {
+		if arr[i] < first {
+			low = append(low, arr[i])
+		} else if arr[i] > first {
+			high = append(high, arr[i])
+		} else {
+			mid = append(mid, arr[i])
+		}
+	}
+	low, high = QuickSort(low), QuickSort(high)
+	myarr := append(append(low, mid...), high...)
+	return myarr
+}
+
+//func HeapSortMax(arr []int, length int) []int {
+//	if length <= 1 {
+//		return arr
+//	}
+//	depth := length/2 - 1
+//	for i := depth; i >= 0; i-- {
+//		topmax := i
+//		leftChild := 2*i + 1
+//		rightChild := 2*i + 2
+//
+//	}
+//}
--- model/user.go
+++ model/user.go
@@ -2,6 +2,10 @@ package model
 
 import "time"
 
+func (User_info) TableName() string {
+	return "user_info"
+}
+
 type User_info struct {
 	UId        string    `json:"u_id"`
 	UserName   string    `json:"user_name"`
